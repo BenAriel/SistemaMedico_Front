84898211df977c3e119ffc07820ae3ff88fca383
BenAriel <fadomarshemello@gmail.com>
2024-07-30 22:53:15 -0300

    animações finais

--- src/rotas/index.tsx
+++ src/rotas/index.tsx
@@ -1,30 +1,82 @@
-import { Route, Routes as Switch, HashRouter } from "react-router-dom";
+import React, { ReactNode } from 'react';
+import { Route, Routes as Switch, HashRouter, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import { AlterarSenha, CadastrarMedicos, Cadastro, ListarAgendamentos, Login, RecuperarAcesso, ListarMedicos, BemVindo, CadastrarPacientes, ListarPacientes, EditarPacientes, EditarAgendamentos } from "../paginas";
 import { AuthProvider } from "../paginas/autenticar/UseAuth";
 import { CadastrarAgendamentos } from "../paginas/agendamentos/CadastrarAgendamentos";
 import { EditarMedicos } from "../paginas/medicos/EditarMedicos";
-export const Rotas = () => {
+
+const pageVariants = {
+    initial: {
+        opacity: 0,
+        x: "-100vw"
+    },
+    animate: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            type: "tween",
+            ease: "anticipate",
+            duration: 0.6
+        }
+    },
+    exit: {
+        opacity: 0,
+        x: "100vw",
+        transition: {
+            type: "tween",
+            ease: "anticipate",
+            duration: 0.6
+        }
+    }
+};
+
+const AnimatedRoutes: React.FC = () => {
+    const location = useLocation();
+
+    return (
+        <AnimatePresence mode="wait">
+            <Switch location={location} key={location.pathname}>
+                <Route path="/agendamentos" element={<MotionWrapper><ListarAgendamentos /></MotionWrapper>} />
+                <Route path="/pacientes" element={<MotionWrapper><ListarPacientes /></MotionWrapper>} />
+                <Route path="/home" element={<MotionWrapper><BemVindo /></MotionWrapper>} />
+                <Route path="/cadastrar" element={<MotionWrapper><Cadastro /></MotionWrapper>} />
+                <Route path="/cadastrarPaciente" element={<MotionWrapper><CadastrarPacientes /></MotionWrapper>} />
+                <Route path="/recuperar" element={<MotionWrapper><RecuperarAcesso /></MotionWrapper>} />
+                <Route path="/alterar" element={<MotionWrapper><AlterarSenha /></MotionWrapper>} />
+                <Route path="/cadastrarMedico" element={<MotionWrapper><CadastrarMedicos /></MotionWrapper>} />
+                <Route path="/cadastrarAgendamento" element={<MotionWrapper><CadastrarAgendamentos /></MotionWrapper>} />
+                <Route path="/medicos" element={<MotionWrapper><ListarMedicos /></MotionWrapper>} />
+                <Route path="/" element={<MotionWrapper><Login /></MotionWrapper>} />
+                <Route path="/editarMedicos" element={<MotionWrapper><EditarMedicos /></MotionWrapper>} />
+                <Route path="/editarPacientes" element={<MotionWrapper><EditarPacientes /></MotionWrapper>} />
+                <Route path="/editarAgendamentos" element={<MotionWrapper><EditarAgendamentos /></MotionWrapper>} />
+            </Switch>
+        </AnimatePresence>
+    );
+};
+
+interface MotionWrapperProps {
+    children: ReactNode;
+}
+
+const MotionWrapper: React.FC<MotionWrapperProps> = ({ children }) => (
+    <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageVariants}
+    >
+        {children}
+    </motion.div>
+);
+
+export const Rotas: React.FC = () => {
     return (
         <AuthProvider>
-        <HashRouter>
-            <Switch>
-                <Route path="/agendamentos" element={<ListarAgendamentos />} />
-                <Route path="/pacientes" element={<ListarPacientes/>} />
-                <Route path="/home" element={<BemVindo/>} />
-                <Route path="/cadastrar" element={<Cadastro />} />
-                <Route path="/cadastrarPaciente" element={<CadastrarPacientes/>} />
-                <Route path="/recuperar" element={<RecuperarAcesso />} />
-                <Route path="/alterar" element={< AlterarSenha/>} />
-                <Route path="/cadastrarMedico" element={<CadastrarMedicos />} />
-                <Route path="/cadastrarAgendamento" element={<CadastrarAgendamentos/>} />
-                <Route path="/medicos" element={<ListarMedicos/>} />
-                <Route path="/" element={<Login />} />
-                <Route path="/editarMedicos" element={<EditarMedicos />} />
-                <Route path="/editarPacientes" element={<EditarPacientes/>} />
-                <Route path="/editarAgendamentos" element={<EditarAgendamentos/>} />
-                
-            </Switch>
-        </HashRouter>
+            <HashRouter>
+                <AnimatedRoutes />
+            </HashRouter>
         </AuthProvider>
     );
 }
